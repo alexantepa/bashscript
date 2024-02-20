bbdea46467fe14c728ecec6c42d93434492234b0
alexantepa <antepa.alex04@gmail.com>
2024-02-20 01:28:35 +0000

    edit task1

--- task1.sh
+++ task1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-file_to_backup="myfile.txt"
+file_to_backup=$1
 backup_file="backup_$(date +'%Y%m%d%H%M%S').txt"
 cp "$file_to_backup" "$backup_file"
 echo "Backup created: $backup_file"
